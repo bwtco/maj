1dbe3a749ac11a7f1452d42fa7f70f128ed6eabf
Sam <sam@synconics.in>
2022-07-06 12:49:52 +0530

    [ADD] Saudi HR

---
+++ saudi_hr/saudi_hr_bonus/models/hr_payroll.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See COPYRIGHT & LICENSE files for full copyright and licensing details.
+
+from odoo import models, fields, api, _
+
+
+class HrPayslip(models.Model):
+    _inherit = "hr.payslip"
+
+    @api.depends('employee_id', 'contract_id', 'struct_id', 'date_from', 'date_to')
+    def _compute_worked_days_line_ids(self):
+        res = super()._compute_worked_days_line_ids()
+        bonus_line_obj = self.env['employee.bonus.lines']
+        bonus_line_ids = bonus_line_obj.search([('employee_id', '=', self.employee_id.id), ('state', 'in', ['done'])])
+        for line in bonus_line_ids:
+            bonus_amount = line.bonus
+            if len(line.period_ids) > 0:
+                bonus_amount = line.bonus / len(line.period_ids)
+            for period in line.period_ids:
+                if period.date_start == self.date_from or period.date_stop == self.date_to:
+                    bonus_type = self.env.ref('saudi_hr_bonus.bonus_input', raise_if_not_found=False)
+                    lines_to_keep = self.input_line_ids.filtered(lambda x: x.input_type_id != bonus_type)
+                    input_lines_vals = [(4, line.id, False) for line in lines_to_keep]
+                    input_lines_vals.append((0, 0, {'amount': bonus_amount,'input_type_id': bonus_type.id}))
+                    self.update({'input_line_ids': input_lines_vals})
+        return res
